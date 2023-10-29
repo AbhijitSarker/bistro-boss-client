402dfcd2a6c0579963352378b3e2b020b50e9b80
AbhijitSarker <abhijitsarker03@gmail.com>
2023-10-29 19:30:39 +0600

    food card and order banner

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -4,8 +4,9 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
 
     const navOptions = <>
-        <Link to='/'><li>Home</li></Link>
-        <Link to='/menu'><li>Menu</li></Link>
+        <li><Link to='/'>Home </Link></li>
+        <li><Link to='/menu'>Our Menu </Link></li>
+        <li><Link to='/order'>Order Food</Link></li>
     </>
     return (
         <div>
