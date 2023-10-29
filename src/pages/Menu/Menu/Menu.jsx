7a8a61ea29d81b4733c22234745f3b31c270a2b9
AbhijitSarker <abhijitsarker03@gmail.com>
2023-10-29 14:48:18 +0600

    usemenu custom hook and banner cover

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,12 +1,18 @@
 import { Helmet } from 'react-helmet-async';
-
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg';
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 const Menu = () => {
     return (
+
+
         <div>
             <Helmet>
                 <title>Our Menu</title>
                 <link rel="canonical" href="https://www.tacobell.com/" />
             </Helmet>
+            <Cover img={menuImg} title={'Our menu'}></Cover>
+
         </div>
     );
 };
