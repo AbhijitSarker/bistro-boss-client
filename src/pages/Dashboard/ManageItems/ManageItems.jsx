615922fb40d5735d33f2836edb5e35561e9a8931
AbhijitSarker <abhijitsarker03@gmail.com>
2023-11-15 23:27:08 +0600

    manage item admin route n load n delete

--- src/pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -1,8 +1,39 @@
+import { FaTrashAlt } from "react-icons/fa";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import useMenu from "../../../hooks/useMenu";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const ManageItems = () => {
-    const [menu] = useMenu();
+    const [menu, , refetch] = useMenu();
+
+    const [axiosSecure] = useAxiosSecure();
+
+    const handleDelete = item => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/menu/${item._id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire(
+                                'Deleted!',
+                                'Your Item has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        });
+    };
 
     return (
         <div className="w-full">
@@ -13,12 +44,10 @@ const ManageItems = () => {
                     {/* head */}
                     <thead>
                         <tr>
-                            <th>
-                                #
-                            </th>
+                            <th>#</th>
                             <th>Name</th>
-                            <th>Job</th>
-                            <th>Job</th>
+                            <th>Price</th>
+                            <th>Category</th>
                             <th>Update</th>
                             <th>Delete</th>
                         </tr>
@@ -27,7 +56,7 @@ const ManageItems = () => {
                         {
                             menu.map((item, index) => <tr key={item._id}>
                                 <th>
-
+                                    {index + 1}
                                 </th>
                                 <td>
                                     <div className="flex items-center gap-3">
@@ -41,15 +70,13 @@ const ManageItems = () => {
                                         </div>
                                     </div>
                                 </td>
-                                <td>
-                                    Zemlak, Daniel and Leannon
-                                </td>
-                                <td>Purple</td>
+                                <td>{item.category}</td>
+                                <td className="text-right">${item.price}</td>
                                 <td>
                                     <button className="btn btn-ghost btn-xs">Update</button>
                                 </td>
                                 <td>
-                                    <button className="btn btn-ghost btn-xs">Delete</button>
+                                    <button onClick={() => handleDelete(item)} className="btn btn-ghost text-white bg-red-600"><FaTrashAlt /></button>
                                 </td>
                             </tr>)
                         }
